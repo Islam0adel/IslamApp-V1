e7f97f75c3467516f184ef177c7689cb66da3075
Islam0adel <sanfor2020@gmail.com>
2026-05-09 02:33:33 +0300

    Fix: Final cleanup and removing large files from tracking

--- frontend/lib/core/constants.dart
+++
@@ -1,9 +0,0 @@
-
-
-class ApiConstants {
-  // لما تحب تشتغل محلي (Local) فك التعليق عن السطر اللي تحت واقفل بتاع Hugging Face
-  // static const String baseUrl = "http://127.0.0.1:8000";
-
-  // رابط Hugging Face (الرابط ده هو اللي هيكون شغال لما ترفع على Vercel)
-  static const String baseUrl = "https://islamadel-islam-app-api.hf.space";
-}
